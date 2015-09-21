dfe9bf874fdb493acde076afc99122be916bc264
Rachel Brunetti <student@otp585596ots.ucsf.edu>
2015-09-21 11:38:54 -0700

    Easier stuff fns RB

--- bootcamp/easier_stuff.py
+++ bootcamp/easier_stuff.py
@@ -14,7 +14,6 @@
 # (don't delete this but don't worry about it either)
 import os # a built-in module, for dealing with filenames
 from . import app # this is part of the website guts
-import csv
 
 
 
@@ -43,50 +42,43 @@ EXPERIMENT_FILE = os.path.join(app.root_path, 'data', 'experiment_data.txt')
 #       [('YAL001C', -0.58), ('YAL002W', 0.23), ('YAL003W', -0.25), ... ],
 #        ... ]
 def experiment():
-	experiment_list=[]
-	with open(EXPERIMENT_FILE) as csvfile:
-		reader = csv.DictReader(csvfile, delimiter='\t')
-		for i in range(0, 32):
-			for row in reader:
-				experiment_list.append([row[''], row[str(i)]])
-	return experiment_list
+	pass
+    
+
 
 # map from a gene's systematic name to its standard name
 # e.g. gene_name('YGR188C') returns 'BUB1'
 def gene_name(gene):
-    pass
+	with open(EXPERIMENT_FILE) as z:
+		for line in z.readlines():
+    		cols = line.split('\t')
+    		if cols[0] == gene:
+    			return cols[1]
 
 
 # map from a gene's systematic name to a list of the values for that gene,
 # across all of the experiments.
 # e.g. gene_data('YGR188C') returns [-0.09, 0.2, -0.07, ... ]
 def gene_data(gene):
-    data_list=[]
-	with open(EXPERIMENT_FILE) as csvfile:
-		reader = csv.DictReader(csvfile, delimiter='\t')
-		for row in reader:
-			if row['']=="%s" % gene:
-				for i in range(0,32):
-					data_list.append(float(row[str(i)]))
-	return data_list
+    pass
 
 
 # map from a systematic name to some info about the gene (whatever you want),
 # e.g  'YGR188C' -> 'Protein kinase involved in the cell cycle checkpoint into anaphase'
 def gene_info(gene):
-    pass
+	with open(EXPERIMENT_FILE) as z:
+    	for line in z.readlines():
+    	cols = line.split('\t')
+    	qualities = cols[2].split(';')
+    	if cols[0] == gene:
+    		return qualities[0]
+
 
 
 # map from a systematic name to a list of GOIDs that the gene is associated with
 # e.g. 'YGR188C' -> ['GO:0005694', 'GO:0000775', 'GO:0000778', ... ]
 def gene_to_go(gene):
-    goid_list=[]
-	with open(GO_MEMBERSHIP) as csvfile:
-		reader = csv.DictReader(csvfile, delimiter='\t')
-		for row in reader:
-			if row['systematic_name']=="%s" % gene:
-				goid_list.append(str(row['goid']))
-	return goid_list
+    pass
 
 
 # map from one of the GO aspects (P, F, and C, for Process, Function, Component),
@@ -99,22 +91,16 @@ def go_aspect(aspect):
 # map from a GOID (e.g. GO:0005737) to a *tuple* of the term, aspect, and term definition
 # e.g. 'GO:0005737' -> ('cytoplasm', 'C', 'All of the contents of a cell... (etc)'
 def go_info(goid):
-    with open(GENE_INFO) as csvfile:
-	reader = csv.DictReader(csvfile, delimiter='\t')
-	for row in reader:
-		if row['goid']=="%s" % goid:
-			goid_tup = (row['go_term'], row['go_aspect'], row['go_definition'])
-	return goid_tup
+	with open(EXPERIMENT_FILE) as z:
+    	for line in z.readlines():
+    	cols = line.split('\t')
+    	if cols[2] == goid:
+    		prop_list.append(cols[0])
+    	return prop_list
 
 
 # the reverse of the gene_to_go function: map from a GOID
 # to a list of genes (systematic names)
 # e.g. 'GO:0005737' -> ['YAL001C', 'YAL002W', 'YAL003W', ... ]
 def go_to_gene(goid):
-    goid_rev_list=[]
-	with open(GO_MEMBERSHIP) as csvfile:
-		reader = csv.DictReader(csvfile, delimiter='\t')
-		for row in reader:
-			if row['goid']=="%s" % goid:
-				goid_rev_list.append(str(row['systematic_name']))
-	return goid_rev_list
+    pass
